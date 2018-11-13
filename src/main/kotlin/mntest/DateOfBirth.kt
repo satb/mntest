7831d7a626329f6cf660507e2bfe5fdd7a9d1f3a
Satish Buddhavarapu <Satish_Buddhavarapu@intuit.com>
2018-11-13 12:46:04 -0800

    small change to the date validation

--- src/main/kotlin/mntest/DateOfBirth.kt
+++ src/main/kotlin/mntest/DateOfBirth.kt
@@ -5,6 +5,6 @@ import javax.validation.constraints.Min
 
 data class DateOfBirth(
   @get:Min(1) @Max(12) val month:String, 
-  @get:Min(1) @Max(30) val date:String,
+  @get:Min(1) @Max(31) val date:String,
   @get:Min(1950) @Max(2018) val year:String 
 )
